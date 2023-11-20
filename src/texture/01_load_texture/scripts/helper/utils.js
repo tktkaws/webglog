8a93e9efa42e4b447c697fd59feb6a57e69135e5
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-11-20 09:03:39 +0900

    add load_texture

---
+++ src/texture/01_load_texture/scripts/helper/utils.js
@@ -0,0 +1,12 @@
+const utils = {
+  lerp,
+};
+
+// 線形補間
+function lerp(a, b, n) {
+  let current = (1 - n) * a + n * b;
+  if (Math.abs(b - current) < 0.001) current = b;
+  return current;
+}
+
+export { utils };
